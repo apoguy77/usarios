589f75ecccb82a43dfe4f910f6a8c428508814f2
Laura Consuelo Galeano Saavedra <lgaleano@adgasd32.int>
2023-09-19 09:58:39 -0500

    guardando

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
 import React, { useState } from 'react';
 import usersData from './users.json';
-import UserList from './UserList'; // Importa el componente UserList
+import UserList from './UserList';
 
-import './styles.css'; // Importa el archivo CSS
+import './styles.css';
+import './App.css';
 
 const App = () => {
   const [name, setName] = useState('');
   const [usuariosBuscados, setUsuariosBuscados] = useState([]);
-  const [selectedUsers, setSelectedUsers] = useState([]); // Estado para las fichas seleccionadas
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
   const buscarUsuarios = () => {
-    // Filtrar usuarios por nombre desde los datos cargados del archivo JSON
+
     const usuariosFiltrados = usersData.users.filter((usuario) =>
       usuario.name.toLowerCase().includes(name.toLowerCase())
     );
 
-    // Actualizar el estado con los usuarios encontrados
+
     setUsuariosBuscados(usuariosFiltrados);
   };
 
   const eliminarUsuario = (usuario) => {
-    // Filtrar los usuarios no seleccionados
     const usuariosRestantes = usuariosBuscados.filter(
       (u) => u !== usuario
     );
 
-    // Actualizar la lista de usuarios buscados
     setUsuariosBuscados(usuariosRestantes);
 
-    // Quitar el usuario de la lista de seleccionados
+
     setSelectedUsers(selectedUsers.filter((u) => u !== usuario));
   };
 
   const toggleUsuarioSeleccionado = (usuario) => {
-    // Comprobar si el usuario ya está seleccionado
     const isSelected = selectedUsers.includes(usuario);
 
     if (isSelected) {
-      // Si ya está seleccionado, quitarlo de la lista de seleccionados
       setSelectedUsers(selectedUsers.filter((u) => u !== usuario));
     } else {
-      // Si no está seleccionado, agregarlo a la lista de seleccionados
       setSelectedUsers([...selectedUsers, usuario]);
     }
   };
 
   return (
-    <div className="container"> {/* Agregamos un contenedor principal */}
-      <h1>Consulta de Usuarios</h1>
+    <div className="container"> {}
+      <h1 className='titulo'>Consulta de Usuarios</h1>
       <div>
         <input
           type="text"
@@ -88,4 +84,3 @@ const App = () => {
 };
 
 export default App;
-  
